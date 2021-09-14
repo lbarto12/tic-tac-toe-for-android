8ac68f39ed3d5958a22b41b22ba9798d71e5f25f
lbarto12 <74741908+lbarto12@users.noreply.github.com>
2021-09-13 23:12:21 -0400

    Final

--- app/src/main/java/com/example/tttfirsttake/MainActivity.kt
+++ app/src/main/java/com/example/tttfirsttake/MainActivity.kt
@@ -1,20 +1,70 @@
 package com.example.tttfirsttake
 
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Display
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.security.acl.NotOwnerException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTitle("Tic Tac Toe")
+
+
+        // LOAD FROM PREFERENCES
+        try {
+            val pref = getPreferences(MODE_PRIVATE)
+            turn1 = pref.getBoolean("turn1", true)
+            autoReset = pref.getBoolean("autoReset", false)
+            val p1temp = pref.getString("p1", "")?.split(" ")
+            val p2temp = pref.getString("p2", "")?.split(" ")
+
+
+            if (p1temp != null && p2temp != null){
+                for (i in p1temp.subList(0, p1temp.size - 1)) p1tiles.add(i.toInt())
+                for (i in p2temp.subList(0, p2temp.size - 1)) p2tiles.add(i.toInt())
+            }
+
+
+
+        } catch (e: Exception){
+            print(e)
+        }
+
+        updateFromRefresh()
+
+
+        // SET WIDGET FUNCTION
+        reset.setOnClickListener {
+            reset()
+        }
+
+        settings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    fun tilePressed(v: View){
+
+    private var p1tiles = ArrayList<Int>()
+    private var p2tiles = ArrayList<Int>()
+    private var turn1: Boolean = true
+
+
+    companion object { var autoReset: Boolean = false }
+
+    fun tilePressed(v: View) {
         val sel: Button = v as Button
 
         logic(when(sel.id){
@@ -32,15 +82,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-    var p1tiles = ArrayList<Int>() // X, Y
-    var p2tiles = ArrayList<Int>()
-
-    var turn1 = true
-
-
-
-    fun logic(c: Int, s: Button){
+    private fun logic(c: Int, s: Button){
 
         if (c in p1tiles || c in p2tiles) {
             Toast.makeText(this, "Slot Occupied!", Toast.LENGTH_SHORT).show()
@@ -48,54 +90,122 @@ class MainActivity : AppCompatActivity() {
         }
 
         val news: TextView = findViewById<TextView>(R.id.news)
-        news.setText(if (!turn1) "O's Turn" else "X's Turn")
+        news.text = if (!turn1) "O's Turn" else "X's Turn"
 
         if (turn1) p1tiles.add(c) else p2tiles.add(c)
         s.text = if (turn1) "O" else "X"
 
         if (checkForWin(if (turn1) p1tiles else p2tiles)) {
-            news.setText(if (turn1) "O's win!" else "X's win!")
-            showResetDialog()
+            news.text = if (turn1) "O's win!" else "X's win!"
+            showResetDialog(if (turn1) "O's win!" else "X's win!")
+            return
+        } else if (p1tiles.size + p2tiles.size == 9){
+            news.text = "Tie Game!"
+            showResetDialog("Tie Game!")
+            return
         }
-
         turn1 = !turn1
     }
 
-    fun checkForWin(p: ArrayList<Int>): Boolean {
-
-        // I'm sure this garbage has a cleaner solution
-        return (p.contains(0) && p.contains(1) && p.contains(2)) ||
-                (p.contains(3) && p.contains(4) && p.contains(5)) ||
-                (p.contains(6) && p.contains(7) && p.contains(8)) ||
-                (p.contains(0) && p.contains(3) && p.contains(6)) ||
-                (p.contains(1) && p.contains(4) && p.contains(7)) ||
-                (p.contains(2) && p.contains(5) && p.contains(8)) ||
-                (p.contains(0) && p.contains(4) && p.contains(8)) ||
-                (p.contains(2) && p.contains(4) && p.contains(6))
-
+    // Just to make checking easier
+    private class ArrayCheck(private var nums: ArrayList<Int>){
+        fun all(vararg n: Int): Boolean{
+            for (i in n){
+                if (i !in nums) return false
+            }
+            return true
+        }
     }
 
-    fun showResetDialog(){
+    private fun checkForWin(p: ArrayList<Int>): Boolean {
+
+        val c = ArrayCheck(p)
+        return  c.all(0, 1, 2) or
+                c.all(3, 4, 5) or
+                c.all(6, 7, 8) or
+                c.all(0, 3, 6) or
+                c.all(1, 4, 7) or
+                c.all(2, 5, 8) or
+                c.all(0, 4, 8) or
+                c.all(2, 4, 6)
+    }
+
+    private fun showResetDialog(s: String){
+        if (MainActivity.autoReset){
+            reset()
+            Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
+            return
+        }
         val d: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
-        d.setTitle("Game Over!")
+        d.setTitle(s)
         d.setMessage("Begin New Game?")
         d.setPositiveButton("yes") { _, _ -> reset()}
+        d.setNegativeButton("no") {_, _ -> closeContextMenu()}
         val a: AlertDialog = d.create()
         a.setCanceledOnTouchOutside(false)
         a.show()
+
+        // LOCK BUTTONS
+        lockButtons(true)
     }
 
-    fun reset(){
+    private fun reset(){
         turn1 = true
         val news: TextView = findViewById<TextView>(R.id.news)
-        news.setText("O's Turn")
+        news.text = "O's Turn"
         p1tiles.clear()
         p2tiles.clear()
         for (i in 0..8){
             val b: Button = findViewById<Button>(R.id.cell_1 + i)
             b.text = ""
         }
+        lockButtons(false)
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        updatePrefs()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        updatePrefs()
+    }
+
+    private fun updatePrefs() {
+        val pref = getPreferences(MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = pref.edit()
+
+        var p1 = ""
+        var p2 = ""
+
+        for (i in p1tiles) p1 += "$i "
+        for (i in p2tiles) p2 += "$i "
+
+        editor.putBoolean("turn1", turn1)
+        editor.putBoolean("autoReset", autoReset)
+        editor.putString("p1", p1)
+        editor.putString("p2", p2)
+
+        editor.apply()
+    }
+
+    private fun updateFromRefresh(){
+        for (i in 0..8){
+            val b: Button = findViewById<Button>(R.id.cell_1 + i)
+            if (i in p1tiles) b.text = "O"
+            else if (i in p2tiles) b.text = "X"
+        }
+        val news: TextView = findViewById<TextView>(R.id.news)
+        news.text = if (turn1) "O's Turn" else "X's Turn"
+
+    }
+
+    private fun lockButtons(bool: Boolean) {
+        for (i in 0..8){
+            val b: Button = findViewById<Button>(R.id.cell_1 + i)
+            b.isEnabled = !bool
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/tttfirsttake/SettingsActivity.kt
@@ -0,0 +1,23 @@
+package com.example.tttfirsttake
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.settings.*
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings)
+
+
+        autoResetSwitch.isChecked = MainActivity.autoReset
+
+        autoResetSwitch.setOnCheckedChangeListener { cb, b ->
+            MainActivity.autoReset = autoResetSwitch.isChecked
+        }
+
+    }
+
+
+}
